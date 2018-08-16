1c12e6ef73ca6a701a642fb3e39730e7b25a7a21
tretos53 <adam.g@outlook.com>
2018-08-16 17:03:30 +0100

    Delete psr_bluetooth.sh

--- psr_bluetooth.sh
+++
@@ -1,9 +0,0 @@
-https://sunnybot.wordpress.com/2017/01/07/dualshock-4-controller-and-pygame/
-
-sudo apt install python3-dev python3-pip
-sudo pip3 install ds4drv
-
-Then in the file “/home/user/.bashrc” – where user is pi by default – add the following line to the file:
-
-1
-sudo ds4drv --daemon --led 000008 --emulate-xpad-wireless &
